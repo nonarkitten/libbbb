0aa857e9bd24dda184601569396e9b8460099d72
Jacques Supcik <jacques.supcik@hefr.ch>
2019-03-21 15:27:45 +0100

    WIP

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -Rf build
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../arm-none-eabi.cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake ..
 make
 cpack --config CPackConfig.cmake
 cpack --config CPackSourceConfig.cmake
