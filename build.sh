7f2e161183c8dd8e7b4a1f28f41cbf8554b3d4d4
Jacques Supcik <jacques.supcik@hefr.ch>
2021-01-02 16:37:42 +0100

    include tooldcain in CmakeLists

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 rm -Rf build
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake ..
+# cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake ..
+cmake ..
 make VERBOSE=1
 cpack --config CPackConfig.cmake
 cpack --config CPackSourceConfig.cmake
