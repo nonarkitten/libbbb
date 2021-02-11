abf204419d5147f9cec2feb314cdac35877edf79
Jacques Supcik <jacques.supcik@hefr.ch>
2021-02-11 14:41:41 +0100

    remove web

--- web/.eleventy.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats([
-    "html",
-    "tar.gz"
-  ]);
-};
--- web/_data/now.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-
-    const d = new Date()
-    return d.toISOString()
-
-}
